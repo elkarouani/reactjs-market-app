a5395bc2be837c6e79d6b761b28509156126816f
KDragon <elkarouani@gmail.com>
2019-01-18 19:09:38 +0100

    Task: editing navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../logo.svg';
+import styled from 'styled-components';
 
 export default class App extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      	<nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
 			{/*https://www.iconfinder.com/icons/1243689/call_phone_icon
 			Creative Commons (Attribution 3.0 Unported);
 			https://www.iconfinder.com/Makoto_msk*/}
@@ -18,12 +19,21 @@ export default class App extends Component {
 				</li>
 			</ul>
 			<Link to="/cart" className="ml-auto">
-				<button>
-					<i className="fas fa-cart-plus" /> 
+				<ButtonContainer>
+					<span  className="mr-2">
+						<i className="fas fa-cart-plus" /> 
+					</span>
 					my cart
-				</button>
+				</ButtonContainer>
 			</Link>
-      </nav>
+      	</nav>
     );
   }
 }
+
+const ButtonContainer = styled.button`
+	text-transform: capitalize;
+	font-size: 1.4rem;
+	background: transparent;
+	border: 0.5rem solid var(--lightBlue);
+`;
