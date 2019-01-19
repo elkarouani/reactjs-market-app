3441aa2620618eb116d03ef04e5401e84515fc0f
KDragon <elkarouani@gmail.com>
2019-01-19 12:41:21 +0100

    Task: separate button styles from the navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../logo.svg';
 import styled from 'styled-components';
+import { ButtonContainer } from "./Button";
 
 export default class App extends Component {
   render() {
@@ -30,23 +31,3 @@ export default class App extends Component {
     );
   }
 }
-
-const ButtonContainer = styled.button`
-	text-transform: capitalize;
-	font-size: 1.4rem;
-	background: transparent;
-	border: 0.05rem solid var(--lightBlue);
-	color: var(--lightBlue);
-	border-radius: 0.5rem;
-	padding: 0.2rem 0.5rem;
-	cursor: pointer;
-	margin: 0.2rem 0.5rem 0.2rem 0;
-	transition: all 0.5s ease-in-out;
-	&:hover {
-		background: var(--lightBlue);
-		color: var(--mainBlue);
-	}
-	&:focus {
-		outline: none;
-	}
-`;
