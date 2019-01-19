b373de7ebb677f6c6a0b28005981e678f8d6d9a7
KDragon <elkarouani@gmail.com>
2019-01-19 13:37:21 +0100

    Task: adding title component & editing on products list component

--- src/components/Product_List.js
+++ src/components/Product_List.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import Product from './Product';
+import Title from './Title';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Product/>
-      </div>
-    );
-  }
+  	state = {
+  		product:[]
+  	}	 
+  	render() {
+    	return (
+    		<React.Fragment>
+				<div className="py-5">
+					<div className="container">
+						<Title name="our" title="products" />
+						<div className="row" />
+					</div>
+				</div>
+			</React.Fragment>
+    	);
+  	}
 }
