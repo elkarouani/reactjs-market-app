aa4bd0d8fad2a0571c8b6a797b03a4d26ff5bd92
KDragon <elkarouani@gmail.com>
2019-01-19 15:13:50 +0100

    Task: adding context module for product data

--- src/components/Product_List.js
+++ src/components/Product_List.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import Product from './Product';
 import Title from './Title';
+import {ProductConsumer} from '../context';
 
 export default class App extends Component {
-  	state = {
-  		product:[]
-  	}	 
   	render() {
     	return (
     		<React.Fragment>
 				<div className="py-5">
 					<div className="container">
 						<Title name="our" title="products" />
-						<div className="row" />
+						<div className="row">
+							<ProductConsumer>
+								{value=>{
+									return value.products.map(product => {
+										return <Product key={product.id} product={product}/>;
+									});
+								}}
+							</ProductConsumer>
+						</div>
 					</div>
 				</div>
 			</React.Fragment>
