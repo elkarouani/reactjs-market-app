92d7bbc76bc4de93c82d8639796fdb24719f43d0
KDragon <elkarouani@gmail.com>
2019-01-21 12:41:04 +0100

    Task: adding add to cart buttons

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,10 +9,18 @@ export default class App extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-        	<div className="img-container p-5" onclick={console.log('clicked')}>
+        	<div className="img-container p-5" onClick={()=>console.log('clicked')}>
         		<Link to="/details">
         			<img src={img} alt="product" className="card-img-top" />
         		</Link>
+            <button className="cart-btn" disabled={inCart ? true : false} 
+              onClick={()=>{console.log('added to the cart');}}>
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>{" "}in cart</p>
+              ) : (
+                <i className="fas fa-cart-plus"/>
+              )}
+            </button>
         	</div>
         </div>
       </ProductWrapper>
