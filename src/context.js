77062f84a0299857af9d338e88c1cd3df740a80c
KDragon <elkarouani@gmail.com>
2019-01-26 13:23:53 +0100

    Task: add product in cart functionality

--- src/context.js
+++ src/context.js
@@ -8,7 +8,8 @@ const ProductContext = React.createContext();
 class ProductProvider extends Component {
 	state = {
 		products: [], 
-		detailProduct: detailProduct
+		detailProduct: detailProduct,
+		cart: [] 
 	};
 
 	componentDidMount() {
@@ -39,7 +40,17 @@ class ProductProvider extends Component {
 	};
 
 	addToCart = (id) => {
-		console.log(`hello from add to cart. Id is ${id}`)
+		let tempProducts = [...this.state.products];
+		const index = tempProducts.indexOf(this.getItem(id));
+		const product = tempProducts[index];
+		product.inCart = true;
+		product.count = 1;
+		const price = product.price;
+		product.total = price;
+
+		this.setState(()=>{
+			return {products: tempProducts, cart: [...this.state.cart, product]};
+		}, ()=>{console.log(this.state)});
 	};
 
 	render() {
