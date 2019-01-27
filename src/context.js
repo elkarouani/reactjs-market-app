b0a42464dc0e1762ce290c288c2e537e59c09187
KDragon <elkarouani@gmail.com>
2019-01-27 14:29:15 +0100

    Task: adding style to the modal

--- src/context.js
+++ src/context.js
@@ -10,7 +10,7 @@ class ProductProvider extends Component {
 		products: [], 
 		detailProduct: detailProduct,
 		cart: [],
-		modalOpen: true,
+		modalOpen: false,
 		modalProduct: detailProduct,
 
 	};
@@ -63,7 +63,7 @@ class ProductProvider extends Component {
 		})
 	};
 
-	closeModal = (id) => {
+	closeModal = () => {
 		this.setState(() => {
 			return {modalOpen: false}
 		})
