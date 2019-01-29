2ea5480209d4448946a7490c7eec29514a01edbb
KDragon <elkarouani@gmail.com>
2019-01-29 11:21:15 +0100

    Task: adding cart item component

---
+++ src/components/Cart/CartList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+export default function CartList() {
+	return (
+		<div>
+			<h1>
+				hello from cart list 
+				<CartItem></CartItem>
+			</h1>
+		</div>
+	);
+}
