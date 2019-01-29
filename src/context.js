aea9738e8d148fad501d6e0aeb9cef2d2c8c70cd
KDragon <elkarouani@gmail.com>
2019-01-29 10:35:23 +0100

    Task: setup cart general functionalities

--- src/context.js
+++ src/context.js
@@ -12,7 +12,9 @@ class ProductProvider extends Component {
 		cart: [],
 		modalOpen: false,
 		modalProduct: detailProduct,
-
+		cartSubTotal; 0,
+		cartTax: 0,
+		cartTotal: 0,
 	};
 
 	componentDidMount() {
@@ -69,6 +71,22 @@ class ProductProvider extends Component {
 		})
 	};
 
+	increment = (id) => {
+		console.log('increment methode');
+	};
+
+	decrement = (id) => {
+		console.log('decrement methode');
+	};
+
+	removeItem = (id) => {
+		console.log('item removed');
+	};
+
+	clearCart = () => {
+		console.log('cart was cleared');
+	};
+
 	render() {
 		return (
 			<ProductContext.Provider value={{
@@ -76,7 +94,11 @@ class ProductProvider extends Component {
 				handleDetail: this.handleDetail,
 				addToCart: this.addToCart,
 				openModal: this.openModal,
-				closeModal: this.closeModal
+				closeModal: this.closeModal,
+				increment: this.increment,
+				decrement: this.decrement,
+				removeItem: this.removeItem,
+				clearCart: this.clearCart
 			}}>
 				{this.props.children}
 			</ProductContext.Provider>
