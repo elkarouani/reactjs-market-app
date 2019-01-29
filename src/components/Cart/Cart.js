bc2fff4737aaa973177a19e986d316ba8a70e149
KDragon <elkarouani@gmail.com>
2019-01-29 11:15:55 +0100

    Task: applying Cart Columns and Empty Cart components on the cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import Title from '../Title';
 import CartColumns from './CartColumns';
+import EmptyCart from "./EmptyCart";
+import {ProductConsumer} from '../../context';
 
-export default class App extends Component {
+export default class Cart extends Component {
   render() {
-    return (
-      <section>
-        <Title name="your" title="cart" />
-        <CartColumns />
-      </section>
-    );
+	return (
+	  <section>
+	  	<ProductConsumer>
+	  		{value => {
+	  			const {cart} = value;
+	  			if(cart.length > 0) {
+	  				return (
+						<React.Fragment>
+							<Title name="your" title="cart" />
+							<CartColumns />
+						</React.Fragment>
+	  				);
+	  			} else {
+	  				return <EmptyCart />;
+	  			}
+	  		}}
+	  	</ProductConsumer>
+	  </section>
+	);
   }
 }
