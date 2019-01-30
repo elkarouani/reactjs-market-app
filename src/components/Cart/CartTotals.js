4c2b16e80130c356ccd79a2ffce33d31cff1c4c4
KDragon <elkarouani@gmail.com>
2019-01-30 11:15:10 +0100

    Task: applying paypal checkout button component to the cart total component

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import PayPalButton from './PayPalButton';
 
-export default function CartTotals({value}) {
-	const {cartSubTotal, cartTax, cartTotal, clearCart} = value;
+export default function CartTotals({value, history}) {
+	const {cartSubTotal, cartTax, cartTotal, clearCart,} = value;
 	return (
 		<React.Fragment>
 			<div className="container">
@@ -23,6 +24,7 @@ export default function CartTotals({value}) {
 							<span className="text-title">total :</span>
 							<strong>$ {cartTotal}</strong>
 						</h5>
+						<PayPalButton total={cartTotal} clearCart={clearCart} history={history}/>
 					</div>
 				</div>
 			</div>
